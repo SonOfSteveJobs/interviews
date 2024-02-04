0f47cc72518bb4b673d03fe64fda8513814b3778
SonOfSteveJobs <evgenev2222@gmail.com>
2024-02-04 12:35:28 +0300

    usePrev2

--- src/usePrevious.tsx
+++ src/usePrevious.tsx
@@ -9,3 +9,18 @@ export function usePrevious<T>(value: T): T | undefined {
 
     return prevRef.current;
 }
+
+/**
+ * React docs: You Might Not Need an Effect
+ * If there is no external system involved (for example, if you want to update a 
+ * component’s state when some props or state change), you shouldn’t need an Effect.
+ */
+
+export function usePrevious2<T>(value: T): T | undefined {
+    const prevValue = useRef<T | undefined>(undefined);
+
+    const temp = prevValue.current;
+    prevValue.current = value;
+
+    return temp;
+}
