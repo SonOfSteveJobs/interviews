336457701c93b231f34357f8becdb9e2e932fdf3
SonOfSteveJobs <evgenev2222@gmail.com>
2024-02-04 12:49:46 +0300

    useToggle

--- src/useToggle.tsx
+++ src/useToggle.tsx
@@ -1,7 +1,7 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 
 export function useToggle(on: boolean): [boolean, () => void] {
     const [toggled, setToggled] = useState(on);
 
-    return [toggled, () => setToggled((prev) => !prev)]
+    return [toggled, useCallback(() => setToggled(prev => !prev), [])]
 }
